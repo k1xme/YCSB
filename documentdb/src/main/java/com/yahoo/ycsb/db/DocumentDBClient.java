310d69d62f537562a45a191a1e9787d1c3704808
Kexi Long <longkexi@gmail.com>
2016-09-03 13:20:27 -0700

    fixed bugs

--- documentdb/src/main/java/com/yahoo/ycsb/db/DocumentDBClient.java
+++ documentdb/src/main/java/com/yahoo/ycsb/db/DocumentDBClient.java
@@ -46,7 +46,7 @@ public class DocumentDBClient extends DB {
     documentClient = new DocumentClient(host, masterKey,
             ConnectionPolicy.GetDefault(), ConsistencyLevel.Session);
     // Initialize test database and collection.
-    getDatabase();
+    getCollection(getProperties().getProperty("documentdb.collectionId", "ycsb"));
   }
 
   @Override
